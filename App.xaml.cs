1c2cf805dac3fe2de368d2873d09d81516dc06d4
Den1 <zeromen945@gmail.com>
2026-03-26 16:30:10 +0200

    Create App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,9 @@
+using System.Windows;
+
+namespace SAMPLauncher
+{
+    public partial class App : Application
+    {
+        // Здесь можно добавить логику проверки обновлений или синглтон-запуска
+    }
+}
